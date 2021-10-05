d0aa6eca81e04ba93dd75f33be88fa09a4af8fed
Jeongkyo Kim <chavitak@gmail.com>
2021-10-05 21:29:13 +0900

    feat(controller): 3개의 숫자를 모두 맞히면 3스트라이크로 게임을 종료한다
    - 기능구현 목록 '1-4. 3개의 숫자를 모두 맞히면 3스트라이크로 게임을 종료한다'를 구현한다.

--- src/main/java/baseball/controller/BaseBallGameController.java
+++ src/main/java/baseball/controller/BaseBallGameController.java
@@ -3,6 +3,8 @@ package baseball.controller;
 import baseball.service.BaseBallGameService;
 import baseball.view.BaseBallGameView;
 
+import java.util.Objects;
+
 public class BaseBallGameController {
     private final BaseBallGameView baseBallGameView;
     private final BaseBallGameService baseBallGameService;
@@ -12,19 +14,32 @@ public class BaseBallGameController {
         this.baseBallGameService = baseBallGameService;
     }
 
+    /**
+     * 랜덤한 숫자를 생성하고 게임을 시행한다. 결과가 3스트라이크가 나올 때까지 시행을 반복한다. 3스트라이크가 나오면 게임을 끝낸다
+     */
     public void run() {
         baseBallGameService.generateRandomNumber();
-        while (true) {
-            playATurn();
+        String result = "";
+        while (!Objects.equals(result, "3스트라이크")) {
+            result = playATurn();
         }
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 끝");
     }
 
-    private void playATurn() {
+    /**
+     * 입력을 받아 생성된 랜덤한 숫자와 비교하는 게임을 한 턴 시행하고 결과값을 문자열로 반환한다
+     *
+     * @return 두 숫자를 비교한 결과값
+     */
+    private String playATurn() {
         String userNumber = baseBallGameView.inputUserNumber();
+        String result = "";
         try {
-            System.out.println(baseBallGameService.computeResult(userNumber));
+            result = baseBallGameService.computeResult(userNumber);
+            System.out.println(result);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+        return result;
     }
 }
